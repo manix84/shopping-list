08d663fd7080d9720cb95316f7132864946410a9
Rob <373278+manix84@users.noreply.github.com>
2026-04-25 13:19:21 +0100

    chore: update .gitignore, add VSCode settings, modify ESLint rules, and enhance package scripts

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,15 @@ export default tseslint.config(
       },
     },
     rules: {
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      quotes: [
+        'error',
+        'single',
+        { allowTemplateLiterals: true, avoidEscape: true },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
       '@typescript-eslint/consistent-type-imports': 'warn',
       'no-console': 'off',
     },
