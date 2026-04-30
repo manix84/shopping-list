2aa2e2cd1a5e808661930f3fd611f4a7938307e7
Rob <373278+manix84@users.noreply.github.com>
2026-04-30 22:48:29 +0100

    feat: add pre-commit hook for linting and update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-  ignores: ['dist/**', 'node_modules/**', 'public/**', 'vite.config.d.ts'],
+  ignores: ['coverage/**', 'dist/**', 'node_modules/**', 'public/**', 'storybook-static/**', 'vite.config.d.ts'],
 }, js.configs.recommended, ...tseslint.configs.recommended, {
   files: ['**/*.{js,mjs,cjs,ts,tsx}'],
   languageOptions: {
@@ -40,4 +40,4 @@ export default tseslint.config({
   languageOptions: {
     globals: globals.node,
   },
-}, storybook.configs["flat/recommended"]);
+}, storybook.configs['flat/recommended']);
